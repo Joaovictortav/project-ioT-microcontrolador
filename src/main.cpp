1809a51862bb6201224458b68fb3b7bbb6153b85
João Victor Tavares <joao.santos@cmtecnologia.com.br>
2022-05-07 20:12:04 -0300

    ajustes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "name";
-const char* password = "password";
+const char* ssid = "HOME OI FIBRA 2.4G";
+const char* password = "Veiaco650650";
 WiFiClient wifiClient;
 
-const char* mqttServer = "broker.hivemq.com";
+const char* mqttServer = "192.168.1.142";
 const int mqttPort = 1883;
 const char* clientId = "mqtt_1";
 
@@ -17,6 +17,11 @@ const char* topicLedAzul = "ledAzul";
 const char* topicLedVermelho = "ledVermelho";
 const char* topicLedVerde = "ledVerde";
 
+unsigned long lastMsg = 0;
+int value = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+
 PubSubClient MQTT(wifiClient);  
 
 //Declaração das Funções
@@ -26,7 +31,7 @@ void conectaMQTT();     //Faz conexão com Broker MQTT
 void recebePacote(char* topic, byte* payload, unsigned int length);
 
 void setup(void) {
-  Serial.begin(115200);
+  Serial.begin(9600);
   conectaWiFi();
   MQTT.setServer(mqttServer, mqttPort); 
   MQTT.setCallback(recebePacote);
@@ -36,59 +41,72 @@ void setup(void) {
   pinMode(ledVerde, OUTPUT);
 }
 
-void loop(void) {    
+void loop(void) {  
+
   mantemConexoes();
-  // MQTT.publish(topicLedVermelho, "1");
+  
+  unsigned long now = millis();
+  if (now - lastMsg > 50) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    MQTT.publish("outTopic", msg);
+  }
+
   MQTT.loop();
 }
 
 void mantemConexoes() {
-    if (!MQTT.connected()) {
-       conectaMQTT(); 
-    }
+
+  if (WiFi.status() != WL_CONNECTED) 
     conectaWiFi(); //se não há conexão com o WiFI, a conexão é refeita
-    delay(2000);
+
+  if (!MQTT.connected()) 
+      conectaMQTT(); 
+
 }
 
 void conectaMQTT() { 
-    while (!MQTT.connected()) {
-        Serial.print("Conectando ao Broker MQTT: ");
-        Serial.println(mqttServer);
-        if (MQTT.connect(clientId)) {
-            Serial.println("Conectado ao Broker com sucesso!");
-            MQTT.subscribe(topicLedAzul);
-            MQTT.subscribe(topicLedVermelho);
-            MQTT.subscribe(topicLedVerde);
-        } 
-        else {
-            Serial.println("Não foi possivel se conectar ao broker.");
-            Serial.println("Nova tentatica de conexao em 5s");
-            delay(5000);
-        }
+
+    Serial.print("Conectando ao Broker MQTT: ");
+    Serial.print(mqttServer);
+    Serial.print("  Port: ");
+    Serial.println(mqttPort);
+
+    if(MQTT.connect(clientId)){
+      Serial.println("Conectado ao Broker com sucesso!");
+      MQTT.subscribe(topicLedAzul);
+      MQTT.subscribe(topicLedVermelho);
+      MQTT.subscribe(topicLedVerde);
+      return;
     }
+
+    Serial.println("Não foi possivel se conectar ao broker.");
+    Serial.println("Nova tentatica de conexao em 3s");
+    delay(3000);
 }
 
 void conectaWiFi() {
-  if (WiFi.status() == WL_CONNECTED) {
-     return;
-  }   
-  Serial.print("Conectando-se na rede: ");
+
   WiFi.begin(ssid, password); // Conecta na rede WI-FI  
+  Serial.print("Conectando-se na rede Wi-Fi: ");
+  Serial.println(ssid); 
+  
   while (WiFi.status() != WL_CONNECTED) {
-      delay(100);
-      Serial.print(".");
+    delay(100);
+    Serial.print(".");
   }
+
   Serial.println();
   Serial.print("Conectado com sucesso, na rede: ");
   Serial.print(ssid);  
-  Serial.print("  IP obtido: ");
+  Serial.print("  IP: ");
   Serial.println(WiFi.localIP()); 
 }
 
 void recebePacote(char* topic, byte* payload, unsigned int length) 
 {
     String msg;
-    //obtem a string do payload recebido
     for(int i = 0; i < length; i++) 
     {
        char c = (char)payload[i];
@@ -99,14 +117,17 @@ void recebePacote(char* topic, byte* payload, unsigned int length)
       digitalWrite(ledVermelho, HIGH);
       digitalWrite(ledAzul, LOW);
       digitalWrite(ledVerde, LOW);
+      value = 0;
     } else if (msg == "2") {
       digitalWrite(ledVermelho, LOW);
       digitalWrite(ledAzul, HIGH);
       digitalWrite(ledVerde, LOW);
+      value = 0;
     } else if (msg == "3") {
       digitalWrite(ledVermelho, LOW);
       digitalWrite(ledAzul, LOW);
       digitalWrite(ledVerde, HIGH);
+      value = 0;
     }
     
     Serial.print("Chegou a seguinte string via MQTT: ");
